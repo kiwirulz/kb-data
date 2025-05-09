6c108313f0d49c4f42e5bd3b29e11c015ba1c1e0
kiwirulz <christophe.cote@gmail.com>
2025-05-09 11:41:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,11 +53,6 @@ else:
     # --- Connexion à la base de données ---
     DB_PATH =  "Donnees_JOUR_ventes.sqlite"
     conn = sqlite3.connect(sqlite_file)
-
-    # Afficher les tables disponibles
-    st.write("Tables disponibles dans la base :")
-    tables = pd.read_sql("SELECT name FROM sqlite_master WHERE type='table';", conn)
-    st.dataframe(tables)
     
     # --- Titre ---
     st.title("Tableau de bord - Kwilu Briques")
